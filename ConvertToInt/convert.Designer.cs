fb767c18b9ac012686551a08da5b24a73dbe79f1
Helton <63302605+Helton1234@users.noreply.github.com>
2020-04-29 15:47:11 -0300

    Conversão em decimal para número inteiro
    Conversão em decimal para número inteiro

---
+++ ConvertToInt/convert.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ConvertToInt
+{
+    partial class Conversor
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.visor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(59, 40);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(110, 20);
+            this.txtBox.TabIndex = 0;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.BackColor = System.Drawing.Color.Transparent;
+            this.btnConvert.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnConvert.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnConvert.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkGray;
+            this.btnConvert.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnConvert.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConvert.Font = new System.Drawing.Font("MS Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnConvert.Location = new System.Drawing.Point(59, 145);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(110, 33);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "Converter";
+            this.btnConvert.UseVisualStyleBackColor = false;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // visor
+            // 
+            this.visor.AutoSize = true;
+            this.visor.Font = new System.Drawing.Font("MS Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.visor.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.visor.Location = new System.Drawing.Point(80, 92);
+            this.visor.Name = "visor";
+            this.visor.Size = new System.Drawing.Size(65, 21);
+            this.visor.TabIndex = 2;
+            this.visor.Text = "Visor";
+            // 
+            // Conversor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(232, 219);
+            this.Controls.Add(this.visor);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtBox);
+            this.Name = "Conversor";
+            this.Opacity = 0.99D;
+            this.ShowIcon = false;
+            this.Text = "ConvertToInt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label visor;
+    }
+}
+
---
+++ ConvertToInt/convert.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvertToInt
+{
+    public partial class Conversor : Form
+    {
+        public Conversor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            decimal num = decimal.Parse(txtBox.Text);
+            Convert.ToInt32(num);
+            visor.Text = num.ToString();
+        }
+    }
+}
